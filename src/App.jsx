10690b3a4a5d2efd1987bb3dc5d7005c88cff5a2
neeru0713 <neerurani1307@gmail.com>
2025-10-30 20:07:24 +0530

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
- 
   const btnClickHandler = (type) => {
     if (type === "inc") {
       setCount((prev) => prev + 1);
@@ -14,16 +13,33 @@ function App() {
   };
 
   return (
-    <>
-      <div className="count">
-        <h1>Counter App</h1>
-        <p>Count: {count}</p>
-        <div className="btn">
-          <button onClick={() => btnClickHandler("inc")}>Increment</button>
-          <button onClick={() => btnClickHandler("dec")}>Decrement</button>
-        </div>
+    <div className="app-container" data-testid="app">
+      <h1 className="title" data-testid="title">
+        Counter App
+      </h1>
+
+      <p className="count-text" data-testid="count">
+        Count: {count}
+      </p>
+
+      <div className="button-container" data-testid="button-container">
+        <button
+          className="increment-btn"
+          data-testid="increment-btn"
+          onClick={() => btnClickHandler("inc")}
+        >
+          Increment
+        </button>
+
+        <button
+          className="decrement-btn"
+          data-testid="decrement-btn"
+          onClick={() => btnClickHandler("dec")}
+        >
+          Decrement
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
